38262f61a462da8c80925bab6c9589ca60ed14ec
Roman Ch <shtafet@gmail.com>
2015-12-12 19:55:40 +0200

    Before lesson 5 commit

---
+++ src/renderEngine/Loader.java
@@ -0,0 +1,7 @@
+package renderEngine;
+
+/**
+ * Created by crs on 07.12.2015.
+ */
+public class Loader {
+}
---
+++ src/renderEngine/Renderer.java
@@ -0,0 +1,7 @@
+package renderEngine;
+
+/**
+ * Created by crs on 08.12.2015.
+ */
+public class Renderer {
+}

e72a5029320188002e3c485973e63e7283482832
Roman Ch <shtafet@gmail.com>
2015-12-17 00:15:12 +0200

    *** Lesson 5 finish.

---
+++ src/shaders/ShaderProgram.java
@@ -0,0 +1,7 @@
+package shaders;
+
+/**
+ * Created by crs on 16.12.2015.
+ */
+public class ShaderProgram {
+}
---
+++ src/shaders/StaticShader.java
@@ -0,0 +1,7 @@
+package shaders;
+
+/**
+ * Created by crs on 17.12.2015.
+ */
+public class StaticShader {
+}
